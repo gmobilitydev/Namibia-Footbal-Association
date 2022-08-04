9e2edf0714bca7b3e2b828e613d35719f0d4d811
hpmouton <hubertthedev@gmail.com>
2022-08-04 12:27:38 +0200

    image loading error

--- app/Http/Controllers/Site/WebsiteController.php
+++ app/Http/Controllers/Site/WebsiteController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Blog\Post;
-
 class WebsiteController extends Controller
 {
     public function latestPosts(){
         $latestPostList = Post::all();
+
         return view('Site.index',['latestPostList'=>$latestPostList]);
     }
 }
