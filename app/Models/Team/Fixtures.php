f7557b3535c923ce282b21b20985d355066eea31
extrinsic-07 <erastusmatheus3@gmail.com>
2022-08-04 11:38:14 +0200

    models

---
+++ app/Models/Team/Fixtures.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Team;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fixtures extends Model
+{
+    use HasFactory;
+}
