8aa63ed7c38a9c9f1a068f9d6a367cdbac021d05
extrinsic-07 <erastusmatheus3@gmail.com>
2022-08-04 16:21:22 +0200

    changes

---
+++ app/Models/Team/Team.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Team;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+    protected $table = 'teams';
+
+    protected $fillabe =[
+        'name',
+        'country',
+        'date_founded',
+        'player_id',
+        'fixture_id',
+        'leagure',
+        'points',
+        'manager'
+
+    ];
+    
+}
