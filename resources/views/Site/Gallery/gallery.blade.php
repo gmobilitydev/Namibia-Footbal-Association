c8d7acaf0716c6aa8203dd1d884018e2437e849a
Shaun Kamesiepo <skamesiepo@gmail.com>
2022-08-05 16:53:24 +0200

    NFA Gallery content

--- resources/views/Site/Gallery/gallery.blade.php
+++ resources/views/Site/Gallery/gallery.blade.php
@@ -46,31 +46,52 @@
             <!-- Section: Design Block -->
             <section class="overflow-hidden text-gray-700">
                 <div class="container px-5 mx-auto lg:pt-24 lg:px-24">
-                    <h2 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12"
+                    <h2 class="text-center text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-24"
                         style="color: hsl(0, 0%, 15%)">
                         Gallery</h2>
-                    <div class="flex flex-wrap -m-1 md:-m-2">
-                        <div class="grid grid-cols-4 gap-4">
+                    <div class="flex flex-wrap -m-1 md:-m-2 ">
+                        <div class="grid grid-cols-3 gap-4">
 
                             @for ($i = 0; $i < 10; $i++)
-                                <div class="mb-4">
+                                <div class="mb-1 mr-1 ml-4">
                                     <div class="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
-                                        <img src="{{ asset('assets/images/caf.jpeg') }}"
-                                            class="max-w-xs hover:scale-110 transition duration-300 ease-in-out hover:opacity-50 transition duration-300 ease-in-out bg-slate-900"
-                                            alt="Louvre" />
+                                      <div class="pop-up-gallery-container">
+                                        <div class="image-container">
+                                          <button class="image-item">
+                                            <img src="{{ asset('assets/images/caf.jpeg') }}"
+                                                class="w-full h-full object-cover max-w-xs hover:scale-110 transition duration-300 ease-in-out hover:opacity-80 transition duration-300 ease-in-out bg-slate-900 rounded-md hover: rounded-md"
+                                                alt="Louvre"/>
+                                          </button>
+                                        </div>
+                                      </div>
+                                        <div class="p-5 flex-col gap-3">
+
+                                            <!-- Badge-->
+                                            <div class="flex items-center gap-2">
+                                            </div>
+
+                                            <!-- Image Description -->
+                                            <div class="flex items-center gap-2 mt-1">
+                                            </div>
+                                        </div>
 
                                     </div>
                                 </div>
                             @endfor
+
                         </div>
                     </div>
                 </div>
 
             </section>
-
-
         </section>
-
     </div>
+
+    <!-- Pop Up Gallery Slider -->
+
+
+
+
+
     <!-- Container for demo purpose -->
 @endsection
