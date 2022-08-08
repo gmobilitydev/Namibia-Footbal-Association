3df22f4530d63fa94425729e57f4adc7a2e54535
kauly-dot-repo <angelapeter2000@gmail.com>
2022-08-08 17:03:53 +0200

    Changed Team Layout on Men's Squads Page

--- resources/views/Site/Men/squads.blade.php
+++ resources/views/Site/Men/squads.blade.php
@@ -43,9 +43,9 @@
                                 <!-- <span class="transition duration-300 group-hover:invisible group-hover:opacity-0">Captain: Team Captain</span> -->
                                 <a href="" class="flex items-center gap-3 lg:invisible absolute left-1 top-0 translate-y-3 transition duration-300 group-hover:visible group-hover:translate-y-0">
                                     <span>See the team </span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 -translate-x-4 transition duration-300 group-hover:translate-x-0" viewBox="0 0 20 20" fill="currentColor">
+                                    <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 -translate-x-4 transition duration-300 group-hover:translate-x-0" viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
+                                    </svg> -->
                                     <!--  -->
                                 </a>
                                 </div>
@@ -63,7 +63,7 @@
 
             </div>
             <div x-show="!open" @click.outside="open = true" class="xs:w-full lg:w-3/4 xs:h-45 lg:h-5/6 bg-white p-4"><h4 class="p-4 text-center text-lg text-slate-400"> CLICK on team to see details</h4> </div>
-            <div x-show="open" @click.outside="open = false" class="xs:w-full lg:w-3/4 xs:h-45 lg:h-5/6 bg-white p-4 bg-slate-100">
+            <div x-show="open" @click.outside="open = true" class="xs:w-full lg:w-3/4 xs:h-45 lg:h-5/6 bg-white p-4 bg-slate-100">
                 <h4 class="p-4 align-center text-lg text-slate-400">Brave Warriors</h4>
                 
                 <!-- Tabs Component -->
@@ -90,14 +90,11 @@
                         <div x-show="openTab === 1" class="rounded bg-white h-48 md:h-screen w-full text-body-color text-base leading-relaxed p-6 overflow-auto" style="display: none;">
                             <div class="grid gap-4 grid-cols-3 grid-rows-3">
                                 
-                                @for ($i = 0; $i < 8; $i++)
-                                <div>
-                                    <div class="group relative w-96">
-                                        <img class="w-3/4 h-full object-cover"
-                                            src="https://www.kindacode.com/wp-content/uploads/2022/06/t-shirt-example.png" />
-
+                                @for ($i = 0; $i < 4; $i++)
+                                <div class="block overflow-hidden group relative rounded-2xl">
+                                    <img class="object-cover w-full h-56 group-hover:h-full duration-500" src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" />
                                         <div
-                                            class="absolute top-0 left-0 w-3/4 h-0 flex flex-col justify-center items-center bg-gray-300 opacity-0 group-hover:h-full group-hover:opacity-70 duration-500">
+                                            class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-gray-300 opacity-0 group-hover:h-full group-hover:opacity-70 duration-500">
                                             <div class="flex flex-row">
                                                 <div class="flex flex-col p-1">
                                                     <h3 class="font-bold text-sm">SENIOR CAPS</h3>
@@ -110,19 +107,24 @@
                                             </div>
                                             <a class="text-black hover:text-white mt-5 px-8 py-3 rounded-full bg-slate-400 hover:bg-amber-500 duration-300" href="#">View Full Stats</a>
                                         </div>
-                                    </div>
-                                    <div>
-                                        <h3 class="text-4xl text-amber-500 font-bold">Player Name</h3>
-                                        <p class="text-2xl text-slate-500">Position</p>
+                                    <div class="p-4 bg-gray-900 group-hover:invisible duration-500">
+                                        <p class="text-xs text-gray-500">website.com</p>
+
+                                        <h5 class="text-lg text-white">Player Name</h5>
+
+                                        <p class="mt-1 text-xs text-gray-500">Position</p>
+
                                     </div>
                                 </div>
                                 @endfor
 
                             </div>
                         </div>
-                        <div x-show="openTab === 2" class="rounded bg-white h-96 w-full text-body-color text-base leading-relaxed p-6" style="display: none;">
-                            <div>
-                                Stats
+                        <div x-show="openTab === 2" class="overflow-auto rounded bg-white h-96 w-full text-body-color text-base leading-relaxed p-6" style="display: none;">
+                            <div class="flex flex-col md:grid gap-4 grid-cols-3 grid-rows-3">
+                                <!-- Stats -->
+                                
+                                <!-- End Stats -->
                             </div>
                         </div>
                     </div>
